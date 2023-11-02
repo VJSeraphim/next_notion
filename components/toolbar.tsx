39be4ba90ba39b6d8b0cef7a2a40b5a74be82345
VJS <minsung_96@naver.com>
2023-11-02 23:27:35 +0900

    Emoji Picker

---
+++ components/toolbar.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import { Doc } from "@/convex/_generated/dataModel"
+import { IconPicker } from "./icon-picker"
+
+interface ToolbarProps {
+    initialData: Doc<"documents">
+    preview?: boolean
+}
+
+export const Toolbar = ({
+    initialData,
+    preview
+}: ToolbarProps) => {
+    return (
+        <div className="pl-[54px] group relative">
+            {!!initialData.icon && !preview && (
+                <div className="flex items-center gap-x-2 group/icon pt-6">
+                    <IconPicker onChange={() => {}}>
+                        <p className="text-6xl hover:opacity-75 transition">
+
+                        </p>
+                    </IconPicker>
+                </div>
+            )}
+        </div>
+    )
+}
